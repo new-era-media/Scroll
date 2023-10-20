61fc8b6b0524c0d77781148a58d6360f808dd82e
Ivan <BahtinovIV>
2023-10-20 20:04:41 +0300

    Удаление не нужного компонента

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  p {{ arrLength }}
+  <div class="hello">      
+    <div class="intersFirst" />
+    <div class="block" v-for="(el, i) in arrLength" :key="el" :id="el"></div>
+    <div class="intersLast" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+import { onMounted, onUpdated, ref, watch, nextTick } from "vue";
+const startArr = [...Array(30).keys()];
+const offset = 110
+const arrLength = ref(startArr);
+let once = false
+let observer;
+
+const initInters = (targetEl, callback) => {
+  let options = {
+    root: document.querySelector(".hello"),
+    rootMargin: "1px",
+    threshold: 1.0,
+  };
+
+  observer = new IntersectionObserver(callback, options);
+  observer.observe(targetEl);
+};
+
+onMounted(() => {
+  init();
+});
+
+
+
+const init = () => {
+  const firstEl = document.querySelector(".intersFirst");
+  const lastEl = document.querySelector(".intersLast");
+
+  const callback = (entries, f,s,g) => {
+    entries.forEach(async (entry) => {
+      if (entry.isIntersecting) {
+        const mock = (timeout = 2000) => {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              const classEl = entry.target.getAttribute("class")
+              switch (classEl) {
+                 case 'intersFirst':
+                    arrLength.value.reverse()
+                    arrLength.value.push(arrLength.value.at(-1)-1, arrLength.value.at(-1)-2)
+                    arrLength.value.reverse()
+                    arrLength.value.splice(arrLength.value.length-2, 2);
+                    document.querySelector(".hello").scrollLeft = document.querySelector(".hello").scrollLeft + offset
+                  break
+                case 'intersLast':
+                  arrLength.value.push(arrLength.value.at(-1)+1, arrLength.value.at(-1)+2);
+                  arrLength.value.splice(0,2);
+                  document.querySelector(".hello").scrollLeft = document.querySelector(".hello").scrollLeft - offset
+
+                  if(!once) {
+                    once = true
+                    initInters(firstEl, callback);
+                  }
+                  break;
+              }
+              resolve()
+            }, timeout);
+          });
+        }
+              
+        try {
+          await mock()
+        } catch(e) {
+          console.log('err', e)
+        }
+      }
+    });
+  };
+
+
+  initInters(lastEl, callback);
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+* {
+  box-sizing: border-box;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.hello {
+  display: flex;
+  overflow-x: scroll;
+  position: relative;
+  padding-left: 20px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.block {
+  min-width: 100px;
+  width: 100px;
+  height: 100px;
+  border: 1px red solid;
 }
 </style>
